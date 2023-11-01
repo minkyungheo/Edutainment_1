687e0636dcd1468b61af72fbf3209589a0d37549
[TAEYEN02] <[xodus2331@naver.com]>
2023-11-01 12:09:56 +0900

    화상통화부분이랑 교육부분 페이지 추가했습니다

--- app/src/main/java/org/koreait/edutainment/MainActivity.java
+++ app/src/main/java/org/koreait/edutainment/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(Bundle saveInstanceState) {
         super.onPostCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
-
-        /*sign = findViewById(R.id.); //회원가입 버튼
-        sign.setOnClickListener(v ->{ //회원가입버튼 클릭시, 회원가입 페이지로 넘어감
+/*
+        sign = findViewById(R.id.SpeakButton);
+        sign.setOnClickListener(v ->{
             Intent intent = new Intent(this,SigupActivity.class);
             startActivity(intent);
         });
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         sign.setOnClickListener(v ->{
             Intent intent = new Intent(this,MainActivity.class);
             startActivity(intent);
-        });
-    }*/ // 버튼 누르면 다른 페이지로 이동
+        });*/
+    }
 
 }
---
+++ app/src/main/java/org/koreait/edutainment/ZoomActivity.java
@@ -0,0 +1,19 @@
+package org.koreait.edutainment;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+
+import org.koreait.edutainment.databinding.ActivityZoomBinding;
+
+public class ZoomActivity extends AppCompatActivity {
+   }

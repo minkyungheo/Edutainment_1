833f31d04e4db2b469db1b7225b3506df7ae5568
[TAEYEN02] <[xodus2331@naver.com]>
2023-11-01 10:46:00 +0900

    메인 페이지로 이동하는 것 까지 완료

--- app/src/main/java/org/koreait/edutainment/LoginActivity.java
+++ app/src/main/java/org/koreait/edutainment/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.koreait.edutainment.databinding.ActivityLoginBinding;
@@ -23,28 +24,11 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(this,SigupActivity.class);
             startActivity(intent);
         });
-    }
-    public void loginbuttonClick(View view) {
-        // 로그인 로직을 처리하고 성공하면 메인 화면으로 전환
-        if (performLogin()) {
-            // 로그인 성공 시 메인 화면으로 전환
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+        sign = findViewById(R.id.loginbutton);
+        sign.setOnClickListener(v ->{
+            Intent intent = new Intent(this,MainActivity.class);
             startActivity(intent);
-
-            // (옵션) 로그인 정보 저장
-            saveLoginCredentials();
-        }
+        });
     }
-
-    private void saveLoginCredentials() {
-        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("isLoggedIn", true);
-        editor.apply();
-    }
-
-    private boolean performLogin() {
-        return true; // 로그인 성공
-    }
-
 }
